bd3d2111c2cf6e2dcb986436d370fdb8598674c7
LiuShuai <2819103739@qq.com>
2025-05-22 15:24:56 +0800

    新增菜品

--- sky-common/src/main/java/com/sky/utils/AliOssUtil.java
+++ sky-common/src/main/java/com/sky/utils/AliOssUtil.java
@@ -61,7 +61,7 @@ public class AliOssUtil {
                 .append("/")
                 .append(objectName);
 
-        log.info("文件上传到:{}", stringBuilder.toString());
+        log.info("文件上传到:{}", objectName.toString());
 
         return stringBuilder.toString();
     }
